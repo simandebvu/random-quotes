c3e3301d466444c6b9af1e097917e1d3bd581879
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-09-18 12:47:56 +0200

    feat: :star: add provider with store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
+
+import Store from './utils/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
